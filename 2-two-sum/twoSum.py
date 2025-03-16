70c3c37d3af4ff74fa9dc82cdb7d86ca15631082
mohammadosman <mohammadhakimosman@outlook.com>
2025-03-15 21:53:58 -0700

    finished two-sum with python

--- 2-two-sum/twoSum.py
+++ 2-two-sum/twoSum.py
@@ -5,6 +5,11 @@ class Solution:
         else:
             ptr_counter = 0
             for i in range(len(nums)):
-                ptr_counter += 1
+                ptr_counter = i + 1
+                curr_i = nums[i]
+                while ptr_counter < len(nums):
+                    if curr_i + nums[ptr_counter] == target:
+                        return [i, ptr_counter]
+                    ptr_counter += 1
                 
                 
