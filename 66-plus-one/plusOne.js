0ac98e7979e35fc15785e40614ad14cb88377629
mohammadosman <mohammadhakimosman@outlook.com>
2025-03-21 22:41:54 -0700

    finished plus one with javascript

--- 66-plus-one/plusOne.js
+++ 66-plus-one/plusOne.js
@@ -4,13 +4,22 @@
  */
 var plusOne = function(digits) {
     let number = 0n;
-       digits.array.forEach((element) => {
-         number += element;
-         number *= 10;
-       });
-       number = digits.map((digit) => {
-         number += digit;
-         number *= 10;
-       });
-       return number;
+    for (let i = 0; i < digits.length; i++) {
+      number += BigInt(digits[i]);
+      number *= BigInt(10);
+    }
+    //   digits.array.forEach((element) => {
+    //     number += element;
+    //     number *= 10;
+    //   });
+    //   number = digits.map((digit) => {
+    //     number += digit;
+    //     number *= 10;
+    //   });
+    number = number / 10n;
+    number++;
+    let answer = [];
+    let split_nums = number.toString().split("");
+    answer = split_nums.map(Number);
+    return answer;
   };
